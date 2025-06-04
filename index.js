d2e6f996a47c2a7d7a598eba643c0f58c97eb452
+ <+>
2025-06-04 11:03:03 +0300

    practicing with variable initializing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this filconst
-const companyName="Scuber"
-let mostProfitableNeighborhood="Chelsea"
-let companyCeo="Susan Smith"
-companyCeo=="Susan Smith"
+const companyName="Scuber";
+let mostProfitableNeighborhood="Chelsea";
+let companyCeo="Susan Smith";
+companyCeo=='Susan Smith';
 
